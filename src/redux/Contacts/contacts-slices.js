7378b19dd9832ace0280d0c2ba299da4401452d9
Elena Ovcharenko <elen_rosee@MacBook-Pro-Ovcarenko.local>
2021-09-22 00:46:03 +0300

    updating state, createReducer usage

--- src/redux/Contacts/contacts-slices.js
+++ src/redux/Contacts/contacts-slices.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createReducer, createSlice } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import {
   changeFilter,
@@ -7,41 +7,41 @@ import {
   fetchContactsSuccess,
 } from './contacts-actions';
 
-const contactsSlice = createSlice({
-  name: 'contacts',
-  initialState: [],
-  reducers: {
-    [addContactSuccess]: (state, { payload }) => state.push(payload),
-    [deleteContactSuccess]: (state, { payload }) =>
-      state.filter(contact => contact.id !== payload),
-  },
-  extraReducers: {
-    [fetchContactsSuccess]: (_state, action) => action.payload,
-  },
-});
-
-const filterSlice = createSlice({
-  name: 'filter',
-  initialState: '',
-  reducers: {
-    [changeFilter](state, action) {
-      state = action.payload;
-    },
-  },
-});
-
-// const contacts = createReducer([], {
-//   [fetchContactsSuccess]: (_, { payload }) => payload,
-//   [addContactSuccess]: (state, { payload }) => [payload, ...state],
-//   [deleteContactSuccess]: (state, { payload }) =>
-//     state.filter(contact => contact.id !== payload),
+// const contactsSlice = createSlice({
+//   name: 'contacts',
+//   initialState: [],
+//   reducers: {
+//     [addContactSuccess]: (state, { payload }) => state.push(payload),
+//     [deleteContactSuccess]: (state, { payload }) =>
+//       state.filter(contact => contact.id !== payload),
+//   },
+//   extraReducers: {
+//     [fetchContactsSuccess]: (_state, action) => action.payload,
+//   },
 // });
 
-// const filter = createReducer('', {
-//   [changeFilter]: (_, { payload }) => payload,
+// const filterSlice = createSlice({
+//   name: 'filter',
+//   initialState: '',
+//   reducers: {
+//     [changeFilter](state, action) {
+//       state = action.payload;
+//     },
+//   },
 // });
+
+const contacts = createReducer([], {
+  [fetchContactsSuccess]: (_, { payload }) => payload,
+  [addContactSuccess]: (state, { payload }) => [payload, ...state],
+  [deleteContactSuccess]: (state, { payload }) =>
+    state.filter(contact => contact.id !== payload),
+});
+
+const filter = createReducer('', {
+  [changeFilter]: (_, { payload }) => payload,
+});
 
 export default combineReducers({
-  contacts: contactsSlice.reducer,
-  filter: filterSlice.reducer,
+  contacts,
+  filter,
 });
